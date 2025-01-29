d33b1ab121f8133dba6d8071c0826743bb60e7f0
abdellahbounab <abdellahbounab@gmail.com>
2025-01-29 06:34:48 -0500

    adding Server Side folder, adding ressources

---
+++ server/Server.hpp
@@ -0,0 +1,29 @@
+
+#pragma once
+
+/*
+    @ressources:
+        # Articles:
+            - https://www.lowtek.com/sockets/
+            - https://beej.us/guide/bgnet/html/index-wide.html
+            - https://ruslanspivak.com/lsbaws-part1/
+            - https://www.ibm.com/docs/en/i/7.2?topic=designs-example-nonblocking-io-select
+            - https://medium.com/@md.abir1203/visual-nuts-and-bolts-of-42-webserver-b381abec4df3
+            - https://medium.com/from-the-scratch/http-server-what-do-you-need-to-know-to-build-a-simple-http-server-from-scratch-d1ef8945e4fa
+            - https://ncona.com/2019/04/building-a-simple-server-with-cpp/
+            - https://www.tutorialspoint.com/cplusplus/cpp_web_programming.htm
+
+        # Videos :
+            - https://www.youtube.com/watch?v=-utm73RxNo4
+            - https://www.youtube.com/watch?v=XXfdzwEsxFk
+            - https://www.youtube.com/watch?v=hJHvdBlSxug
+            - https://www.youtube.com/watch?v=3b_TAYtzuho
+            - https://www.youtube.com/watch?v=9J1nJOivdyw
+
+
+*/
+
+
+class Server {
+
+};
